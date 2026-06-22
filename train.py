7e32fbbd29ef181c4c2beff624f4f374194b762b
Barsha0 <dhakalbarsha519@gmail.com>
2026-06-22 20:43:37 +0545

    Overfitting adjustment

--- train.py
+++ train.py
@@ -1,8 +1,11 @@
 import tensorflow as tf 
 from clean_data import clean_dataset
+from reduce_overfitting import get_data_augmentation
 
 # Clean the dataset first
 clean_dataset()
+
+# The model learns from training data.
 
 train_data = tf.keras.utils.image_dataset_from_directory(
     "PetImages",
@@ -14,6 +17,8 @@ train_data = tf.keras.utils.image_dataset_from_directory(
     label_mode = "binary"
 )
 
+# The model never trains on validation data, only gets tested on it.
+
 val_data = tf.keras.utils.image_dataset_from_directory(
     "PetImages",
     image_size = (224, 224),
@@ -24,6 +29,9 @@ val_data = tf.keras.utils.image_dataset_from_directory(
     label_mode = "binary"
 )
 
+# Data Augmentatiom
+
+data_augmentation = get_data_augmentation()
 
 #Build model 
 
@@ -31,6 +39,7 @@ model = tf.keras.Sequential([
 
     # Layer one: normalize pixels 0-225 -> 0 - 1
     tf.keras.layers.Input(shape = ((224, 224, 3))),
+    data_augmentation,
     tf.keras.layers.Rescaling(1./255),
 
 
@@ -49,10 +58,12 @@ model = tf.keras.Sequential([
 
     #Flatten -> squishes the 3D to 1D
     tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.5),           # Dropout to rreduce overfitting
 
     # Last Layer
-    tf.keras.layers.Dense(128, activation = 'relu'),      #thinking layer -> think and combine feature given by flatten(in one line)
-    tf.keras.layers.Dense(1, activation= 'sigmoid')      #one output neuron -> 0 or 1 (binary) -> Cat or Dog?
+    tf.keras.layers.Dense(128, activation = 'relu'),     # thinking layer -> think and combine feature given by flatten(in one line)
+    tf.keras.layers.Dropout(0.3),                        # Dropout to rreduce overfitting
+    tf.keras.layers.Dense(1, activation= 'sigmoid')      # one output neuron -> 0 or 1 (binary) -> Cat or Dog?
 ])
 
 # Compile Model
